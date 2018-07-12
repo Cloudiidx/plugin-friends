7684ace757ccdfc522258b3e9746709927189907
jasonhaxstuff <shadowmanguyyt@gmail.com>
2018-07-12 17:15:10 -0400

    make some changes, still only adds the add subcommand.

--- src/commands/friends/FriendCommand.ts
+++ src/commands/friends/FriendCommand.ts
@@ -16,9 +16,11 @@ friend requests [incoming/outgoing]
 
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando'
 import { Message, User } from 'discord.js'
+import { oneLine } from 'common-tags'
+import { User as NightwatchUser } from '@nightwatch/db'
 import axios from 'axios'
 
-module.exports = class FriendCommand extends Command {
+export default class FriendCommand extends Command {
   constructor(client: CommandoClient) {
     super(client, {
       name: 'friend',
@@ -26,15 +28,14 @@ module.exports = class FriendCommand extends Command {
       memberName: 'friend',
       description:
         'Allows you to send and respond to friend requests, as well as list your friends/friend requests.',
-      details: `
+      details: oneLine`
         \`friend add <mention>\` sends a friend request to that user.\n
         \`friend accept <mention>\` accepts a friend request from that user.\n
         \`friend deny/decline <mention>\` denies a friend request from that user.\n
         \`friend remove/delete <mention>\` removes that user from your friend list.\n
         \`friend list [mention]\` lists all the user's friends, or your own if no user is given.\n
         \`friend requests [incoming/outgoing]\` lists all of your incoming or outgoing friend requests, respectfully.
-        If no type is specified, it will list both incoming and outgoing friend requests.
-			`,
+        If no type is specified, it will list both incoming and outgoing friend requests.`,
       examples: [
         'friend add @Joker#3650',
         'friend deny @Joker#3650',
@@ -55,8 +56,7 @@ module.exports = class FriendCommand extends Command {
           label: 'user or type',
           prompt: 'Please provide a valid argument for the used subcommand.',
           default: -1,
-          type: 'user|string',
-          infinite: false
+          type: 'user|string'
         }
       ]
     })
@@ -64,32 +64,50 @@ module.exports = class FriendCommand extends Command {
 
   async run(
     msg: CommandMessage,
-    { subcommand, argument }: { subcommand: string; argument: User | string }
-  ): Promise<Message> {
-    if (subcommand === 'add') {
-      if (!(argument instanceof User)) {
-        return msg.reply(
-          'This command requires you to specify a user. Please try again.'
-        ) as Promise<Message>
-      }
+    {
+      subcommand,
+      argument
+    }: { subcommand: string; argument: User | 'incoming' | 'outgoing' }
+  ): Promise<Message | Message[]> {
+    switch (subcommand.toLowerCase()) {
+      case 'add':
+        return this.sendFriendRequest(msg, argument)
 
-      const sender = await axios.get(
-        `http://localhost:5000/api/users/${msg.author.id}`
-      )
-      const receiver = await axios.get(
-        `http://localhost:5000/api/users/${argument.id}`
-      )
+      default:
+        return msg.reply(`\`${subcommand}\` is not a valid subcommand.`)
+    }
+  }
 
-      await axios.post(
-        `http://localhost:5000/api/users/${argument.id}/friends/requests`,
-        { user: sender.data, receiver: receiver.data }
-      )
+  async sendFriendRequest(
+    msg: CommandMessage,
+    argument: User | 'incoming' | 'outgoing'
+  ): Promise<Message | Message[]> {
+    const receiver = await this.getApiUser(argument)
+    const sender = await this.getApiUser(msg.author)
 
-      return msg.reply(`Sent a friend request to ${argument.tag}`) as Promise<
-        Message
-      >
+    if (receiver === null) {
+      return msg.reply(
+        'This command requires you to specify a user. Please try again.'
+      )
     }
 
-    return msg.delete()
+    await axios.post(
+      `http://localhost:5000/api/users/${receiver.id}/friends/requests`,
+      { user: sender, receiver: receiver }
+    )
+
+    return msg.reply(`Sent a friend request to **${receiver.name}**.`)
+  }
+
+  async getApiUser(user: any): Promise<NightwatchUser | null> {
+    if (user instanceof User) {
+      const response = await axios.get(
+        `http://localhost:5000/api/users/${user.id}`
+      )
+
+      return response.data
+    }
+
+    return null
   }
 }
